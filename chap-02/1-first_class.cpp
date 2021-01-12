06893f55caad5ee592115fb73ff07525926a0549
YohanRameau <yohan.rameau@gmail.com>
2021-01-12 23:49:21 +0100

    Finish chap2 first exo

--- chap-02/1-first_class.cpp
+++ chap-02/1-first_class.cpp
@@ -1,13 +1,40 @@
 #include <iostream>
+#include <string>
+class Person
+{
+private:
+    std::string    _name;
+    unsigned short _age = 0;
+
+public:
+    void set_name(const std::string& name)
+    {
+        _name = name;
+    }
+    void set_age(unsigned short age)
+    {
+        _age = age;
+    }
+
+    std::string get_name(void) const
+    {
+        return _name;
+    }
+    int get_age(void) const
+    {
+        return _age;
+    }
+};
 
 int main()
 {
-    // Person p;
+    Person p;
 
-    // p.set_name("Batman");
-    // p.set_age(23);
+    p.set_name("Batman");
+    p.set_age(23);
 
-    // std::cout << "Person named '" << p.get_name() << "' is " << p.get_age() << " years old." << std::endl;
+    std::cout << "Person named '" << p.get_name() << "' is " << p.get_age() << " years old." << std::endl;
 
     return 0;
 }
+
